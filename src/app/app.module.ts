ecfa20f583e7d11ce345dc7003d110c64411d4a8
Kaleb <kaleb.wond@gmail.com>
2021-06-16 22:55:04 +0300

    hello done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent,
+    UserListComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule
