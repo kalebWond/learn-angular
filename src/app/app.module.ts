3bf1b39285c7a056880b647974511a1eeb37ad29
Kaleb Wondwossen <kaleb.wond@gmail.com>
2021-06-25 11:21:53 +0300

    inventory done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductImageComponent,
+    ProductRowComponent,
+    ProductDepartmentComponent,
+    PriceDisplayComponent
   ],
   imports: [
     BrowserModule
